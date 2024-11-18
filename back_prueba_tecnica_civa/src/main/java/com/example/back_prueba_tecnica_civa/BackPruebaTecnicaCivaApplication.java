767f11349104eb836583dcf5a9d273b3866eb248
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-11-18 16:13:31 -0500

    First commit

---
+++ back_prueba_tecnica_civa/src/main/java/com/example/back_prueba_tecnica_civa/BackPruebaTecnicaCivaApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_prueba_tecnica_civa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPruebaTecnicaCivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPruebaTecnicaCivaApplication.class, args);
+	}
+
+}
---
+++ back_prueba_tecnica_civa/src/test/java/com/example/back_prueba_tecnica_civa/BackPruebaTecnicaCivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_prueba_tecnica_civa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPruebaTecnicaCivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
