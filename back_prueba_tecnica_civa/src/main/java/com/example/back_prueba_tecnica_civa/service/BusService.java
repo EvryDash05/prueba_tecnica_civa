cc23a2215b1f68089dcb8b136b544d9af53eb4b4
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-11-18 19:09:10 -0500

    Se corrigen los métodos

--- back_prueba_tecnica_civa/src/main/java/com/example/back_prueba_tecnica_civa/service/BusService.java
+++ back_prueba_tecnica_civa/src/main/java/com/example/back_prueba_tecnica_civa/service/BusService.java
@@ -2,12 +2,13 @@ package com.example.back_prueba_tecnica_civa.service;
 
 import com.example.back_prueba_tecnica_civa.models.request.BusRequest;
 import com.example.back_prueba_tecnica_civa.models.response.BusResponse;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface BusService {
-    List<BusResponse> getAllBuses();
+    List<BusResponse> getAllBuses(Pageable pageable);
     BusResponse createBus(BusRequest busRequest);
-    BusRequest getBusById(Integer busId);
+    BusResponse getBusById(Integer busId);
     void deleteBysById(Integer busId);
 }
