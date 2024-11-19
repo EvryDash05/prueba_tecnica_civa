a40a88394aae9b91c40e79454f0d711a46a74520
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-11-18 20:32:58 -0500

    Se añade la configuración de Spring Security

---
+++ back_prueba_tecnica_civa/src/main/java/com/example/back_prueba_tecnica_civa/security/utils/JwtUtils.java
@@ -0,0 +1,74 @@
+package com.example.back_prueba_tecnica_civa.security.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtils {
+
+    @Value("${security.jwt.key}")
+    private String privateKey;
+
+    @Value("${security.jwt.user.generator}")
+    private String userGenerator;
+
+    public String createToken(Authentication authentication) {
+        Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+        String username = authentication.getPrincipal().toString();
+        String authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        return JWT.create()
+                .withIssuer(this.userGenerator)
+                .withSubject(username)
+                .withClaim("authorities", authorities)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date (System.currentTimeMillis() + 1800000))
+                .withJWTId(UUID.randomUUID().toString())
+                .withNotBefore(new Date(System.currentTimeMillis()))
+                .sign(algorithm);
+    }
+
+    public DecodedJWT verifyToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+            JWTVerifier jwtVerifier = JWT.require(algorithm)
+                    .withIssuer(this.userGenerator)
+                    .build();
+
+            return jwtVerifier.verify(token);
+        } catch (Exception e) {
+            throw new JWTVerificationException("Token invalid, not authorized");
+        }
+    }
+
+    public String extractUsername(DecodedJWT decodedJWT) {
+        return decodedJWT.getSubject().toString();
+    }
+
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName) {
+        return decodedJWT.getClaim(claimName);
+    }
+
+    public Map<String, Claim> returnClaims(DecodedJWT decodedJWT){
+        return decodedJWT.getClaims();
+    }
+
+}
