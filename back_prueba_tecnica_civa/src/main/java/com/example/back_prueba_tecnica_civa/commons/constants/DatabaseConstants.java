505fcaf38eb7b4c034c2d792262333c7a6389ee7
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-11-18 19:52:30 -0500

    Se agregan las constantes para las nuevas tablas

--- back_prueba_tecnica_civa/src/main/java/com/example/back_prueba_tecnica_civa/commons/constants/DatabaseConstants.java
+++ back_prueba_tecnica_civa/src/main/java/com/example/back_prueba_tecnica_civa/commons/constants/DatabaseConstants.java
@@ -22,5 +22,47 @@ public abstract class DatabaseConstants {
     public static final String BRAND_ID = "id";
     public static final String BRAND_NAME = "brand_name";
 
+    /**
+     * <h2>Role table 🛢</h2>
+     */
+    /* Property for the table name of table "tb_roles" */
+    public static final String ROLE_TABLE = "tb_role";
+
+    /* Property for the primary key on table "tb_roles" */
+    public static final String ROLE_ID = "role_id";
+
+    /* Property for the role name on table "tb_roles" */
+    public static final String ROLE_NAME = "role_name";
+
+    /**
+     * <h2>Customer table 🛢</h2>
+     */
+    /* Property for the table name of "tb_user" */
+    public static final String USER_TABLE = "tb_user";
+
+    /* Property for the primary key on table "tb_user" */
+    public static final String USER_ID = "user_id";
+
+    /* Property for the username column on table "tb_user" */
+    public static final String USERNAME = "username";
+
+    /* Property for the email column on table "tb_user" */
+    public static final String EMAIL = "email";
+
+    /* Property for the password column on table "tb_user" */
+    public static final String PASSWORD = "password";
+
+    /**
+     * <h2>Customer Role table 🛢</h2>
+     */
+    /* Property for the table name of "user_role" */
+    public static final String USER_ROLE_TABLE = "user_role";
+
+    /* Property for the customer ID column on table "user_role" */
+    public static final String USER_ROLE_USER_ID = "user_id";
+
+    /* Property for the role ID column on table "user_role" */
+    public static final String USER_ROLE_ROLE_ID = "role_id";
+
     private DatabaseConstants() {}
 }
